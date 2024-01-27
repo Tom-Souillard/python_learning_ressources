be2b9522e3019e4e252ecc8b6fc97d8d2cf494a9
Tom Souillard <tom.souillard@gmail.com>
2024-01-27 09:27:30 +0100

    feat(get_user_category): Ajout de la fonction get_user_category pour la selection des catégories
    - Affichage des catégories récupérés du fichier sentence_to_translate.py
    - Permet à l’utilisateur de choisir parmi les catégories disponibles
    - Gestion des entrées utilisateur avec validation pour s’assurer que le choix est dans la plage valide.

--- py_english_practice/py_english_pratice.py
+++ py_english_practice/py_english_pratice.py
@@ -1,7 +1,28 @@
+import sentence_to_translate
+
+def get_user_category():
+    categories = sentence_to_translate.get_categories()
+    print("Chosissez une catégorie : ")
+    print("0. Toutes les catégories")
+    for index, category in enumerate(categories,1):
+        print(f"{index}. {category}")
+
+    while True:
+        try:
+            choice = int(input(": "))
+            if 0 <= choice <= len(categories):
+                return choice
+            else:
+                print(f"Entrée invalide. Veuillez choisir un nombre entre 0 et {len(categories)}")
+        except ValueError:
+            print(f"Entrée invalide. Veuillez choisir un nombre entre 0 et {len(categories)}")
+
+
+
 def get_user_mode():
     while True:
         try:
-            mode = int(input("Choisissez une option : \n1. Commencer le Quiz\n2. Consulter l’historique des erreurs\n: "))
+            mode = int(input("Choisissez une option : \n0. Commencer le Quiz\n1. Consulter l’historique des erreurs\n: "))
             if mode in [0,1]:
                 return mode
             print("Entrée invalide. Veuillez choisir 0 ou 1")
@@ -11,6 +32,8 @@ def get_user_mode():
 def main():
     print("Bienvenue dans le Quiz d’anglais")
     user_mode = get_user_mode()
+    user_category = get_user_category()
+
 
 if __name__ == "__main__":
     main()
