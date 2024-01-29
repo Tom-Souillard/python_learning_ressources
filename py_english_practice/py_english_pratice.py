011bbf098ac89b13c2ca479ae84b61879365c511
Tom Souillard <tom.souillard@gmail.com>
2024-01-29 08:36:32 +0100

    feat(main-application): Étendue des fonctionnalités principales et amélioration de l’interface utilisateur
    - Amélioration de la fonction start_quiz() pour inclure une sélection aléatoire de phrases et un mécanisme de correction basé sur le choix de l’utilisateur.
    - Ajout de la fonction dispaly_quiz_text() pour une meilleure présentation visuelle des différentes section du quiz.
    - Implémentation de la fonction save_errors_to_json pour enregistrer les erreurs des utilisateurs dans un fichier JSON, permettant un suivi et une révision efficaces.
    - Ajout de la fonction view_history() pour afficher l’historique des erreurs trié par fréquence
    - Renforcement de la validation des entrées utilisateur dans get_user_category() et get_user_mode()

--- py_english_practice/py_english_pratice.py
+++ py_english_practice/py_english_pratice.py
@@ -1,4 +1,91 @@
+import random
 import sentence_to_translate
+import json
+import os
+
+def view_history():
+    try:
+        with open("error_history.json", "r") as file:
+            errors_dict = json.load(file)
+    except FileNotFoundError:
+        print("Aucun historique d’erreur trouvé.")
+        return
+
+    sorted_errors = sorted(errors_dict.items(), key=lambda item: item[1]['count'], reverse=True)
+
+    print(f"\n{'Nb erreurs':<11} | {'Français': <60} | {'Anglais': <60}")
+    print("-"*110)
+
+    for french_sentence, details in sorted_errors:
+        nb_errors = details['count']
+        english_sentence = details['en']
+        print(f"{nb_errors: <11} | {french_sentence: <60} | {english_sentence: <60}")
+
+def display_quiz_text(text):
+    ascii_border = """
+    ***********************
+    *{}*
+    ***********************
+    """.format(text)
+    print(ascii_border)
+
+def save_errors_to_json(new_errors, filename="error_history.json"):
+    if os.path.exists(filename):
+        with open(filename, "r") as file:
+            existing_errors = json.load(file)
+        for key, value in new_errors.items():
+            if key in existing_errors:
+                existing_errors[key]['count'] += value['count']
+            else:
+                existing_errors[key] = value
+    else:
+        existing_errors = new_errors
+
+    with open(filename, "w") as file:
+        json.dump(existing_errors, file, indent=4, ensure_ascii=False)
+
+def start_quiz():
+    feedback_mode = int(input("Souhaitez-vous une correction après chaque question?\n1. Oui\n0. Non\n: "))
+    user_category = get_user_category()
+    number_of_questions = int(input("Combien de questions voulez-vous? \n: "))
+
+    if user_category == "all":
+        sentences = []
+        for category in sentence_to_translate.get_categories():
+            sentences.extend(sentence_to_translate.get_sentences(category))
+    else:
+        sentences = sentence_to_translate.get_sentences(user_category)
+    selected_sentences = random.sample(sentences, min(number_of_questions, len(sentences)))
+
+
+    display_quiz_text("     Quiz Start      ")
+
+    correct_answer = 0
+    incorrect_answers_dic = {}
+
+    for i, sentence in enumerate(selected_sentences, 1):
+        print(f"\n{i}. {sentence['fr']}")
+        user_answer = input(": ")
+        if user_answer == sentence['en']:
+            print("*** Correct! ***")
+            correct_answer += 1
+        else:
+            if feedback_mode:
+                print(f": {sentence['en']}")
+            print("Incorrect!")
+            incorrect_answers_dic[sentence['fr']] = {'en': sentence['en'], 'count':1}
+
+    save_errors_to_json(incorrect_answers_dic)
+    display_quiz_text("      Quiz End       ")
+    print(f"\nNombre de bonnes réponses : {correct_answer}/{number_of_questions}")
+
+    if incorrect_answers_dic:
+        print("\nDétail des erreurs :")
+        for i, (french_sentence, details) in enumerate(incorrect_answers_dic.items(), start=1):
+            english_translation = details['en']
+            print(f"{i}. {french_sentence}\n>> {english_translation}\n")
+
+
 
 def get_user_category():
     categories = sentence_to_translate.get_categories()
@@ -11,7 +98,10 @@ def get_user_category():
         try:
             choice = int(input(": "))
             if 0 <= choice <= len(categories):
-                return choice
+                if choice == 0:
+                    return "all"
+                else:
+                    return categories[choice - 1]
             else:
                 print(f"Entrée invalide. Veuillez choisir un nombre entre 0 et {len(categories)}")
         except ValueError:
@@ -22,18 +112,27 @@ def get_user_category():
 def get_user_mode():
     while True:
         try:
-            mode = int(input("Choisissez une option : \n0. Commencer le Quiz\n1. Consulter l’historique des erreurs\n: "))
+            mode = int(input("Choisissez une option : \n1. Commencer le Quiz\n0. Consulter l’historique des erreurs\n: "))
             if mode in [0,1]:
                 return mode
-            print("Entrée invalide. Veuillez choisir 0 ou 1")
+            print("Entrée invalide. Veuillez choisir 1 ou 0")
         except ValueError:
-            print("Entrée invalide. Veuillez choisir 0 ou 1")
+            print("Entrée invalide. Veuillez choisir 1 ou 0")
 
 def main():
-    print("Bienvenue dans le Quiz d’anglais")
-    user_mode = get_user_mode()
-    user_category = get_user_category()
+    while True:
+        display_quiz_text(" Welcome to the Quiz ")
+        user_mode = get_user_mode()
 
+        if user_mode:
+            start_quiz()
+        else:
+            view_history()
+
+        restart = int(input("\n**********************\nVoulez-vous revenir au menu principal ?\n1. Oui\n0. Non\n: "))
+        if restart == False:
+            print("Merci d’avoir utilisé le quiz. Au revoir.")
+            break
 
 if __name__ == "__main__":
     main()
